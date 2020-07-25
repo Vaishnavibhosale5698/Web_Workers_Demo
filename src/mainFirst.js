32200a10687ca4d19f4a3511368c65cf42521ad0
Vaishnavi Bhosale <vaishnavibhosale5698@gmail.com>
2020-07-25 15:58:41 +0530

    Producer-Consumer Problem

--- src/mainFirst.js
+++
@@ -1,9 +0,0 @@
-const worker = new Worker("../src/worker.js");
-
-worker.onmessage = e => {
-  const message = e.data;
-  console.log(`[From Worker]: ${message}`);
-//   const reply = setTimeout(() => worker.postMessage("Marco!"), 3000);
-};
-
-worker.postMessage("Marco!");
--- src/worker.js
+++
@@ -1,7 +0,0 @@
-onmessage = e => {
-    const message = e.data;
-    console.log(`[From Main]: ${message}`);
-    // const reply = setTimeout(() => postMessage("Polo!"), 3000);
-  };
-
-//   postMessage("Polo!");
